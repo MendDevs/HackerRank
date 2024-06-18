1bbfcb332976e199e16331e98df4389f5386024d
Emmanuel Markie Morris <164094648+MendDevs@users.noreply.github.com>
2024-06-18 13:49:14 +0100

    Update Palindrome Index.java

--- Prepare/Interview Preparation Kits/1 Week Preparation Kit/Day 3/Palindrome Index.java
+++ Prepare/Interview Preparation Kits/1 Week Preparation Kit/Day 3/Palindrome Index.java
@@ -1,7 +1,8 @@
 /////////////////////////////// Palindrom Index by Hacker Rank ///////////////////////////
 
 import java.io.*;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 
 class Result {
 
@@ -11,42 +12,33 @@ class Result {
      * The function is expected to return an INTEGER.
      * The function accepts STRING s as parameter.
      */
+     
+    private static boolean isPali(char[] chars, int l, int r){
+        while(l < r) {
+            if(chars[l] != chars[r]) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
     
-    // Function to find the index of the character to remove to make the string a palindrome
     public static int palindromeIndex(String s) {
-        int n = s.length();
-        
-        // Iterate from the start to the middle of the string
-        for (int i = 0; i < n / 2; i++) {
-            // If characters at current index and its mirror index are not equal
-            if (s.charAt(i) != s.charAt(n - 1 - i)) {
-                // Check if removing the character at the current index forms a palindrome
-                if (isPalindrome(s, i + 1, n - 1 - i)) {
+        int length = s.length();
+        char[] ch = s.toCharArray();
+        for(int i = 0; i < length / 2; i++) {
+            if(ch[i] != ch[length - 1 - i]) {
+                if(isPali(ch, i + 1, length - 1 - i)) {
                     return i;
-                // Check if removing the character at the mirror index forms a palindrome
-                } else if (isPalindrome(s, i, n - 2 - i)) {
-                    return n - 1 - i;
+                } else if(isPali(ch, i, length - 2 - i)) {
+                    return length - 1 - i;
                 } else {
-                    // If neither removal forms a palindrome, return -1
                     return -1;
                 }
             }
         }
-        
-        // If the string is already a palindrome or no single character removal can make it a palindrome
-        return -1;
-    }
-
-    // Helper function to check if a substring s[left:right+1] is a palindrome
-    private static boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
+        return -1; // If the string is already a palindrome
     }
 }
 
@@ -55,19 +47,14 @@ public class Solution {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        // Read the number of queries
         int q = Integer.parseInt(bufferedReader.readLine().trim());
 
-        // Process each query
         IntStream.range(0, q).forEach(qItr -> {
             try {
-                // Read the string for the current query
                 String s = bufferedReader.readLine();
 
-                // Get the result from the palindromeIndex function
                 int result = Result.palindromeIndex(s);
 
-                // Write the result to the output
                 bufferedWriter.write(String.valueOf(result));
                 bufferedWriter.newLine();
             } catch (IOException ex) {
@@ -75,9 +62,9 @@ public class Solution {
             }
         });
 
-        // Close the bufferedReader and bufferedWriter
         bufferedReader.close();
         bufferedWriter.close();
     }
 }
+
 
